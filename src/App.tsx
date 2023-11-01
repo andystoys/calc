bd45827a597dc9272fbdd0756ccfc2a38605691f
Andy Pak <64394640+andystoys@users.noreply.github.com>
2023-10-31 23:48:31 -0700

    Add percentage calculator functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import 'bulma/css/bulma.min.css';
+
+function formatPrice(number: number, percentage: number): string {
+  let formatted = round9(markup(number, percentage))
+  return isNaN(formatted) ? "" : formatted.toString()
+}
+
+function round9(number: number): number {
+  let cent = number * 100 % 10 * 0.01
+  let diff = 0.09 - cent
+  let formatted = diff <= 0.04 ? number + diff : number - cent - 0.01
+  // console.log("number: " + number + "cent: " + cent + "diff: " + diff + "formatted: " + formatted)
+  return formatted >= 0 ? parseFloat(formatted.toFixed(2)) : 0
+}
+
+// 100% is max and will be infinite
+function markup(price: number, percentage: number): number {
+  let value = parseFloat((price / (1 - percentage * 0.01)).toFixed(2))
+  return value
+}
+
+function formatNumber(number: string): string {
+  const filtered = number.replace(/\./g, '')
+  if (filtered.length > 2) {
+    const prefix = filtered.slice(0, -2)
+    const suffix = filtered.slice(-2)
+    return `${prefix}.${suffix}`
+  } else {
+    return filtered
+  }
+}
 
 function App() {
+  const percentages1 = []
+  const percentages2 = []
+  for (let i = 5; i <= 100; i += 5) {
+    if (i <= 50) percentages1.push(i)
+    else percentages2.push(i)
+  }
+
+  const [inputValue, setInputValue] = useState('');
+  const handleInputChange = (input: { target: { value: React.SetStateAction<string>; }; }) => {
+    let number = input.target.value.toString()
+    setInputValue(formatNumber(number))
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h1 className="title">Beer🍺 % Calculator</h1>
+
+      <div className="tile is-ancestor">
+        <div className="tile is-3 is-vertical is-parent">
+          <div className="tile is-child">
+            <input
+              autoFocus
+              className="input is-medium"
+              onChange={handleInputChange}
+              pattern="[0–9]*"
+              placeholder="$ Amount"
+              step=".01"
+              type="number"
+              value={inputValue}
+            />
+          </div>
+
+        </div>
+        <div className="tile is-parent">
+          <div className="tile is-child">
+            <ul>
+              {percentages1.map((percentage, index) => (
+                <li key={index}>{percentage}%: → ${formatPrice(parseFloat(inputValue), percentage)} </li>
+              ))}
+            </ul>
+          </div>
+          <div className="tile is-child">
+          <ul>
+            {percentages2.map((percentage, index) => (
+              <li key={index}>{percentage}%: → ${formatPrice(parseFloat(inputValue), percentage)} </li>
+            ))}
+            </ul>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
